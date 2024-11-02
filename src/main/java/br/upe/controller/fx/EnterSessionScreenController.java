9b4d19a1f712ddb8a39890a6d742d9ed5a7dd10b
kio <kaio.viana@upe.br>
2024-11-01 23:31:17 -0300

    Fix some visuals

--- src/main/java/br/upe/controller/fx/EnterSessionScreenController.java
+++ src/main/java/br/upe/controller/fx/EnterSessionScreenController.java
@@ -4,7 +4,6 @@ import br.upe.controller.AttendeeController;
 import br.upe.controller.SessionController;
 import br.upe.controller.UserController;
 import br.upe.persistence.Persistence;
-import br.upe.persistence.User;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
@@ -18,7 +17,8 @@ import java.util.Optional;
 
 public class EnterSessionScreenController extends BaseController implements FxController{
     private UserController userController;
-    AttendeeController attendeeController;
+    private AttendeeController attendeeController;
+    private SessionController sessionController;
 
     @FXML
     private VBox attendeeVBox;
@@ -29,9 +29,11 @@ public class EnterSessionScreenController extends BaseController implements FxCo
     @FXML
     private AnchorPane attendeePane;
 
+
     public void setUserController(UserController userController) throws IOException {
         this.userController = userController;
         this.attendeeController = new AttendeeController();
+        this.sessionController = new SessionController();
         initial();
     }
 
@@ -55,9 +57,6 @@ public class EnterSessionScreenController extends BaseController implements FxCo
     public void handleSession() throws IOException {
         genericButton("/fxml/sessionScreen.fxml", attendeePane, userController, null);
     }
-    public void handleAddSession() throws IOException{
-        genericButton("/fxml/sessionScreen.fxml",attendeePane, userController,null);
-    }
 
 
     public void logout() throws IOException {
@@ -75,6 +74,8 @@ public class EnterSessionScreenController extends BaseController implements FxCo
 
         attendeeVBox.setAlignment(Pos.CENTER);
 
+        Map<String, Persistence> sessionHashMap = sessionController.getSessionHashMap();
+
         for (Map.Entry<String, Persistence> entry : attendeeController.getAttendeeHashMap().entrySet()) {
             Persistence persistence = entry.getValue();
             if (persistence.getData("userId").equals(userController.getData("id"))) {
@@ -84,6 +85,8 @@ public class EnterSessionScreenController extends BaseController implements FxCo
 
                 Label eventLabel = new Label(persistence.getData("name"));
                 eventLabel.setStyle("-fx-font-size: 20px; -fx-text-fill: #333333;");
+
+                Label sessionLabel = createSessionLabel(persistence.getData("sessionId"), sessionHashMap);
 
                 Button editButton = new Button("Editar");
                 editButton.setStyle("-fx-background-color: #6fa3ef; -fx-text-fill: white; -fx-cursor: hand; -fx-effect: dropshadow(three-pass-box, rgba(128, 128, 128, 1), 3.88, 0, -1, 5);");
@@ -102,7 +105,7 @@ public class EnterSessionScreenController extends BaseController implements FxCo
 
                 deleteButton.setOnAction(e -> {
                     try {
-                        handleDeleteAttendee(persistence.getData("id"), userController.getData("id"),persistence.getData("sessionId"));
+                        handleDeleteAttendee(userController.getData("id"),persistence.getData("sessionId"));
                     } catch (IOException ex) {
                         throw new IllegalArgumentException(ex);
                     }
@@ -112,16 +115,27 @@ public class EnterSessionScreenController extends BaseController implements FxCo
                 actionButtons.setAlignment(Pos.CENTER_RIGHT);
                 actionButtons.getChildren().addAll(editButton, deleteButton);
 
-                eventContainer.getChildren().addAll(eventLabel, actionButtons);
+                eventContainer.getChildren().addAll(eventLabel, actionButtons, sessionLabel);
 
                 attendeeVBox.getChildren().add(eventContainer);
             }
         }
     }
+
+    private Label createSessionLabel(String eventId, Map<String, Persistence> sessionHashMap) {
+        Label sessionLabel = new Label();
+        String nameEvent = (sessionHashMap != null && sessionHashMap.containsKey(eventId))
+                ? sessionHashMap.get(eventId).getData("name")
+                : "Evento não encontrado";
+        sessionLabel.setText(nameEvent);
+        sessionLabel.setStyle("-fx-font-size: 18px; -fx-text-fill: #555555;");
+        return sessionLabel;
+    }
+
     private void handleEditAttendee(String eventName) throws IOException {
         genericButton("/fxml/updateAttendeeScreen.fxml", attendeePane, userController, eventName);
     }
-    private void handleDeleteAttendee(String eventId, String userId, String sessionId) throws IOException {
+    private void handleDeleteAttendee(String userId, String sessionId) throws IOException {
         Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
         confirmationAlert.setTitle("Confirmação de Exclusão");
         confirmationAlert.setHeaderText("Deseja realmente excluir este participante?");
